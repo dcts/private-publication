b2c80178ae8b4748b1759370a7b0b60daf03cb28
dcts <thomas@liist.com>
2022-07-19 22:41:18 +0200

    :white_check_mark: Ex1 Step1 green

--- dnas/lobby/coordinator_zomes/private_publication_lobby/src/lib.rs
+++ dnas/lobby/coordinator_zomes/private_publication_lobby/src/lib.rs
@@ -1,4 +1,21 @@
+use hdk::prelude::{holo_hash::AgentPubKeyB64, *};
 
+#[derive(Serialize, Deserialize, Debug, SerializedBytes)]
+struct Properties {
+    progenitor: AgentPubKeyB64,
+}
+
+#[hdk_extern]
+fn progenitor(_: ()) -> ExternResult<AgentPubKey> {
+    let properties = dna_info()?.properties;
+    
+    let progenitor_properties = Properties::try_from(properties)
+        .map_err(|err| {
+            wasm_error!(err.into())
+        })?;
+
+    Ok(progenitor_properties.progenitor.into())
+}
 /** Don't change */
 #[cfg(feature = "exercise")]
 extern crate private_publication_lobby;
